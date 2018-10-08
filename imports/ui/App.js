411e8c0cef490002a79ae05dda667aeca0678cbf
Jon Urry <jon@urry.me>
2018-10-08 16:20:24 +0100

    3. collections

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,21 +1,14 @@
 import React, { Component } from 'react';
+import { withTracker } from 'meteor/react-meteor-data';
+
+import { Tasks } from '../api/tasks.js';
 
 import Task from './Task.js';
 
 // App component - represents the whole app
-export default class App extends Component {
-  getTasks() {
-    return [
-      { _id: 1, text: 'This is task 1' },
-      { _id: 2, text: 'This is task 2' },
-      { _id: 3, text: 'This is task 3' },
-    ];
-  }
-
+class App extends Component {
   renderTasks() {
-    return this.getTasks().map((task) => (
-      <Task key={task._id} task={task} />
-    ));
+    return this.props.tasks.map(task => <Task key={task._id} task={task} />);
   }
 
   render() {
@@ -25,10 +18,14 @@ export default class App extends Component {
           <h1>Todo List</h1>
         </header>
 
-        <ul>
-          {this.renderTasks()}
-        </ul>
+        <ul>{this.renderTasks()}</ul>
       </div>
     );
   }
 }
+
+export default withTracker(() => {
+  return {
+    tasks: Tasks.find({}).fetch()
+  };
+})(App);
